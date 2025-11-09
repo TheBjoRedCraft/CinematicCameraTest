d4fa9c0057e07112a73011fb3dd44922024e0d61
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-11-09 12:44:50 +0100

    Implement player join listener and camera path functionality

--- src/main/kotlin/dev/thebjoredcraft/test/PaperMain.kt
+++ src/main/kotlin/dev/thebjoredcraft/test/PaperMain.kt
@@ -1,9 +1,14 @@
 package dev.thebjoredcraft.test
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import dev.slne.surf.surfapi.bukkit.api.event.register
+import dev.thebjoredcraft.test.listener.PlayerJoinListener
 import org.bukkit.plugin.java.JavaPlugin
 
 val plugin get() = JavaPlugin.getPlugin(PaperMain::class.java)
 
 class PaperMain : SuspendingJavaPlugin() {
+    override fun onEnable() {
+        PlayerJoinListener.register()
+    }
 }
